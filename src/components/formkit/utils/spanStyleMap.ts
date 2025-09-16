04d719913053f37fc4da2a6316e3442af4aa65df
kubilay <kubilay.turgut@gmail.com>
2025-09-16 13:06:20 +0900

    formkit x gap updated

--- src/components/formkit/utils/spanStyleMap.ts
+++ src/components/formkit/utils/spanStyleMap.ts
@@ -6,7 +6,7 @@ const spanStyleMap: {
   "3": "calc(33.33% - 13.33px)", // 33.33% - (gap*2/3)
   "4": "calc(25% - 15px)",    // 25% - (gap*3/4)
   "5": "calc(20% - 16px)",    // 20% - (gap*4/5)
-  "6": "calc(16.67% - 16.67px)", // 16.67% - (gap*5/6)
+  "6": "calc(16.67% - 17.67px)", // 16.67% - (gap*5/6)
 }
 
 export default spanStyleMap
