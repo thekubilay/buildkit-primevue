8c40e7edfb63aafab5284433961df2d2beaaa617
kubilay <kubilay.turgut@gmail.com>
2025-10-06 13:51:10 +0900

    buildkit custom styles added

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -27,6 +28,12 @@ export default defineConfig({
         exports: 'named',
         globals: {
           vue: 'Vue'
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') {
+            return 'styles.css';
+          }
+          return assetInfo.name || '';
         }
       }
     }
