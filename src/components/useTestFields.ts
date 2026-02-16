7504771b1a361c06e434bfa953c5bc6a274706a5
kubilay <kubilay.turgut@gmail.com>
2026-02-16 11:02:24 +0900

    input clearing issue fix

--- src/components/useTestFields.ts
+++ src/components/useTestFields.ts
@@ -11,36 +11,38 @@ const useTestFields = () => {
       colSpan: {mobile: 1, tablet: 2, desktop: 2}, // Responsive
       class: "w-full",
     },
-    type: {
-      inputId: "type",
-      label: "種類",
-      as: "Select",
-      defaultValue: 'tel',
-      placeholder: "選択してください",
-      schema: "required", // Required, max 12 chars, hiragana only
-      colSpan: {mobile: 1, tablet: 1, desktop: 1}, // Responsive
+    zipcode: {
+      inputId: "zipcode",
+      label: "Zipcode",
+      defaultValue: '5380042',
+      placeholder: "000000",
+      schema: "required",
+      colSpan: {mobile: 1, tablet: 2, desktop: 2},
       class: "w-full",
-      options: ["email", "tel"]
     },
-    email: {
-      inputId: "email_id",
-      label: "メールアドレス",
+    prefecture: {
+      inputId: "prefecture",
+      label: "氏名",
       defaultValue: '',
-      placeholder: "email@example.com",
-      schema: "required|email|max:100", // Required, email format, max 100 chars
-      colSpan: {mobile: 1, tablet: 1, desktop: 1}, // Responsive
+      placeholder: "氏名を入力",
+      colSpan: {mobile: 1, tablet: 2, desktop: 2}, // Responsive
       class: "w-full",
-      showWhen: {field: "type", equals: "email"},
     },
-    phone: {
-      inputId: "phone_id",
-      label: "電話番号",
+    address: {
+      inputId: "address",
+      label: "氏名",
       defaultValue: '',
-      placeholder: "09012345678",
-      schema: "required|number", // Required, numbers only, 10-11 digits
-      colSpan: {mobile: 1, tablet: 1, desktop: 1}, // Responsive
+      placeholder: "氏名を入力",
+      colSpan: {mobile: 1, tablet: 2, desktop: 2}, // Responsive
       class: "w-full",
-      showWhen: {field: "type", equals: "tel"},
+    },
+    address2: {
+      inputId: "address2",
+      label: "氏名",
+      defaultValue: '',
+      placeholder: "氏名を入力",
+      colSpan: {mobile: 1, tablet: 2, desktop: 2}, // Responsive
+      class: "w-full",
     },
   })
 
